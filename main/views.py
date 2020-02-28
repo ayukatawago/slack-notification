0610300f632967d59af08edaa6dcda674fbe454e
ayukatawago <46732412+ayukatawago@users.noreply.github.com>
2020-02-28 10:21:30 +0900

    initial Slack Api implementation

--- main/views.py
+++ main/views.py
@@ -1,6 +1,11 @@
 from main import app
+from bot.slackbot import SlackBot
+import config
 
 
 @app.route('/')
 def show_entries():
-    return 'Hello, World!'
+    message = 'Hello, World!'
+    slack_bot = SlackBot(config.slack_api_token)
+    slack_bot.post_message(channel="#todo", message=message)
+    return message
