6064b0d6d7265beb09796d0c0c715d70bf679271
ayukatawago <46732412+ayukatawago@users.noreply.github.com>
2020-03-06 10:04:25 +0900

    complete Trello card at Slack Button action

--- flaskr/views.py
+++ flaskr/views.py
@@ -3,6 +3,7 @@ from flask import request, make_response
 import config
 from flaskr import app
 from slackutil.slackbot import SlackBot
+from trelloutil.trellowrapper import TrelloWrapper
 
 
 @app.route("/")
@@ -14,13 +15,17 @@ def show_entries():
 @app.route("/slack/json_html", methods=["POST"])
 def json_html():
     slack_client = SlackBot(config.slack_api_token)
+    trello_client = TrelloWrapper(config.trello_api_key, config.trello_token)
 
     # Parse the request payload
     form_json = json.loads(request.form["payload"])
 
-    val = form_json["actions"][0]["value"]
+    print(json.dumps(form_json))
+    card_id = form_json["actions"][0]["value"]
+    channel_id = form_json["channel"]["id"]
+    trello_client.complete_card(card_id)
 
-    message = "complete card_id:" + val
-    slack_client.post_message("#todo", message)
+    message = "complete card_id:" + card_id
+    slack_client.post_message(channel_id, message)
 
     return make_response("", 200)
