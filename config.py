74aa3a6f37b0e1ede4125dabed4e10dbde0bbde9
ayukatawago <46732412+ayukatawago@users.noreply.github.com>
2020-03-18 08:57:21 +0900

    add Google Calendar notification

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 import os
 
 slack_api_token = os.environ.get("SLACK_API_TOKEN")
-slack_channel = os.environ.get("SLACK_CHANNEL")
+slack_trello_channel = os.environ.get("SLACK_TRELLO_CHANNEL")
+slack_calendar_channel = os.environ.get("SLACK_CALENDAR_CHANNEL")
 
 trello_api_key = os.environ.get("TRELLO_API_KEY")
 trello_token = os.environ.get("TRELLO_TOKEN")
 trello_board = os.environ.get("TRELLO_BOARD")
+
+google_credential = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+target_calendars = os.environ.get("TARGET_CALENDARS")
